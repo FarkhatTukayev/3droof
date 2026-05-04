04e0a7843e925a22341dfe76823d8b8121e5e0dc
Farkhat Tukayev <farhattukaev@gmail.com>
2026-05-04 20:21:53 +0500

    Update app.listen to bind to 0.0.0.0

--- server.js
+++ server.js
@@ -131,6 +131,6 @@ app.post('/api/calculate', (req, res) => {
         detailedMaterials
     });
 });
-app.listen(PORT, () => {
-    console.log(`RoofCalc API is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`RoofCalc API is running on port ${PORT}`);
 });
